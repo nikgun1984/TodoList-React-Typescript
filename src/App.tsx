6658afa9eeed2fa45872c441a99b8d8b490d6b08
Nick Gundobin <ngundobin@gmail.com>
2021-03-26 12:17:29 -0400

    handle removing todo

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import NewTodo from './components/NewTodo';
 import {useState} from "react";
 import {Todo} from './todo.models';
 const App: React.FC = () => { // can also be FunctionalComponent/ClassicComponent
+  // our state with generics using interface Todo array
   const [todos, setTodos] = useState<Todo[]>([]);
   
   const addTodo = (text: string) => {
     setTodos(prevTodos=>[...prevTodos, {id: Math.random().toString(), text:text}]);
   }
+  const toDoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos=>{
+      return prevTodos.filter(todo=> todo.id===todoId)
+    })
+  };
   return (
     <div className="App">
-      <NewTodo addTodo = {addTodo} />
-      <TodoList items={todos}/>
+      <NewTodo addTodo = {addTodo}/>
+      <TodoList items={todos} removeTodo={toDoDeleteHandler}/>
     </div>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,14 @@
 interface TodoListProps {
 	items: {id: string, text:string}[];
+	removeTodo: (id:string)=>void;
 }
 
-const TodoList: React.FC<TodoListProps> = ({items}) => {
+const TodoList: React.FC<TodoListProps> = ({items, removeTodo}) => {
 	return <ul>
-		{items.map(todo=><li key={todo.id}>{todo.text}</li>)}
+		{items.map(todo=><li key={todo.id}>
+			<span>{todo.text}</span>
+			<button onClick={removeTodo.bind(null,todo.id)}>Delete</button>
+		</li>)}
 	</ul>
 }
 
